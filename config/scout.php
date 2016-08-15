6a16f88315226e1f06bd6c76e8c2b288e3808b32
Nenad Ticaric <nticaric@gmail.com>
2016-08-15 02:53:00 +0200

    add config

--- config/scout.php
+++ config/scout.php
@@ -46,4 +46,12 @@ return [
         'secret' => env('ALGOLIA_SECRET'),
     ],
 
+    'tntsearch' => [
+        'driver'   => env('DB_CONNECTION', 'mysql'),
+        'host'     => env('DB_HOST', 'localhost'),
+        'database' => env('DB_DATABASE'),
+        'username' => env('DB_USERNAME'),
+        'password' => env('DB_PASSWORD'),
+        'storage'  => storage_path(),
+    ],
 ];
