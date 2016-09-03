d7a8ced776adca4869e2efd6779e47a3ec79ade7
Ivan Zhuravlev <intech@users.noreply.github.com>
2016-09-03 16:04:28 +0300

    Update AlgoliaEngine.php

--- src/Engines/AlgoliaEngine.php
+++ src/Engines/AlgoliaEngine.php
@@ -20,6 +20,16 @@ class AlgoliaEngine extends Engine
     }
 
     /**
+     * Get Elasticsearch client instance.
+     *
+     * @return \Elasticsearch\Client
+     */
+    public function getClient()
+    {
+        return $this->elasticsearch;
+    }
+
+    /**
      * Update the given model in the index.
      *
      * @param  \Illuminate\Database\Eloquent\Collection  $models
