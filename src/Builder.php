86a6227e832ba5e6b585d49464aa2ee49501446d
Harlequin Doyon <harlequin.doyon@gmail.com>
2016-09-10 14:14:57 +0800

    Return length aware paginator

--- src/Builder.php
+++ src/Builder.php
@@ -2,6 +2,7 @@
 
 namespace Laravel\Scout;
 
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -121,7 +122,7 @@ class Builder
      * @param  int  $perPage
      * @param  string  $pageName
      * @param  int|null  $page
-     * @return \Illuminate\Contracts\Pagination\Paginator
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
      */
     public function paginate($perPage = 15, $pageName = 'page', $page = null)
     {
@@ -133,13 +134,12 @@ class Builder
             $rawResults = $engine->paginate($this, $perPage, $page), $this->model
         ));
 
-        $paginator = (new Paginator($results, $perPage, $page, [
+        $paginator = (new LengthAwarePaginator($results, $engine->getTotalCount($rawResults), $perPage, $page, [
             'path' => Paginator::resolveCurrentPath(),
             'pageName' => $pageName,
         ]));
 
-        return $paginator->appends('query', $this->query)
-                         ->hasMorePagesWhen(($engine->getTotalCount($rawResults) / $perPage) > $page);
+        return $paginator->appends('query', $this->query);
     }
 
     /**
