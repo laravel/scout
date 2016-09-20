1a0461a95f9871edeef4dbbb4ab32bdb9c2b6ad9
Brandon Shar <bshar@thatsus.com>
2016-09-20 10:40:57 -0500

    dont call searchable functions on an empty collection

--- src/Searchable.php
+++ src/Searchable.php
@@ -48,6 +48,10 @@ trait Searchable
      */
     public function queueMakeSearchable($models)
     {
+        if ($models->isEmpty()) {
+            return;
+        }
+
         if (! config('scout.queue')) {
             return $models->first()->searchableUsing()->update($models);
         }
@@ -64,6 +68,10 @@ trait Searchable
      */
     public function queueRemoveFromSearch($models)
     {
+        if ($models->isEmpty()) {
+            return;
+        }
+        
         return $models->first()->searchableUsing()->delete($models);
     }
 
