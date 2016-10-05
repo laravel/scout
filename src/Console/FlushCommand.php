e0eff31c5443a83c8e5ae3778cb67297970c0ae8
Taylor Otwell <taylorotwell@gmail.com>
2016-10-05 10:30:00 -0500

    files

---
+++ src/Console/FlushCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Laravel\Scout\Console;
+
+use Illuminate\Console\Command;
+
+class FlushCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scout:flush {model}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Flush all of the model's records from the index";
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $class = $this->argument('model');
+
+        (new $class)::removeAllFromSearch();
+
+        $this->info('All ['.$class.'] records have been flushed.');
+    }
+}
