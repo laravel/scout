235eb735e3206406e11ff0ab093f46dde5eb7e02
Dorin Niscu <dorin.niscu@gmail.com>
2017-06-02 13:10:34 +0300

    Added numericFilters / facetFilters methods.

--- src/Engines/AlgoliaEngine.php
+++ src/Engines/AlgoliaEngine.php
@@ -74,8 +74,8 @@ class AlgoliaEngine extends Engine
     public function search(Builder $builder)
     {
         return $this->performSearch($builder, array_filter([
-            'facetFilters' => $this->filters($builder, 'facet'),
-            'numericFilters' => $this->filters($builder, 'numeric'),
+            'facetFilters' => $this->facetFilters($builder),
+            'numericFilters' => $this->numericFilters($builder),
             'hitsPerPage' => $builder->limit,
         ]));
     }
@@ -91,8 +91,8 @@ class AlgoliaEngine extends Engine
     public function paginate(Builder $builder, $perPage, $page)
     {
         return $this->performSearch($builder, [
-            'facetFilters' => $this->filters($builder, 'facet'),
-            'numericFilters' => $this->filters($builder, 'numeric'),
+            'facetFilters' => $this->facetFilters($builder),
+            'numericFilters' => $this->numericFilters($builder),
             'hitsPerPage' => $perPage,
             'page' => $page - 1,
         ]);
@@ -127,19 +127,42 @@ class AlgoliaEngine extends Engine
      * Get the filter array for the query.
      *
      * @param  \Laravel\Scout\Builder  $builder
-     * @param  string $filterType
      * @return array
      */
-    protected function filters(Builder $builder, $filterType)
+    protected function filters(Builder $builder)
     {
-        $collection = collect($builder->wheres)->filter(function ($where) use ($filterType) {
-            if ($filterType == 'numeric') {
-                return is_int($where);
-            } else if ($filterType == 'facet') {
-                return is_string($where);
-            }
+        return collect($builder->wheres)->map(function ($value, $key) {
+            return $key.'='.$value;
+        })->values()->all();
+    }
 
-            return false;
+    /**
+     * Get the numeric filter array for the query.
+     *
+     * @param  \Laravel\Scout\Builder  $builder
+     * @return array
+     */
+    protected function numericFilters(Builder $builder)
+    {
+        $collection = collect($builder->wheres)->filter(function ($where) {
+            return is_int($where);
+        });
+
+        return $collection->map(function ($value, $key) {
+            return $key.'='.$value;
+        })->values()->all();
+    }
+
+    /**
+     * Get the facet filter array for the query.
+     *
+     * @param  \Laravel\Scout\Builder  $builder
+     * @return array
+     */
+    protected function facetFilters(Builder $builder)
+    {
+        $collection = collect($builder->wheres)->filter(function ($where) {
+            return is_string($where);
         });
 
         return $collection->map(function ($value, $key) {
