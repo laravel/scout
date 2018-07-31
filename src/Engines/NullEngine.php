1082245c345482b4cc5aca4639f97f924fb005d0
Hoglan <Hoglan@users.noreply.github.com>
2018-07-31 16:07:04 +0100

    Update NullEngine.php
    Fixing the map function from amends a few days ago,

--- src/Engines/NullEngine.php
+++ src/Engines/NullEngine.php
@@ -67,11 +67,12 @@ class NullEngine extends Engine
     /**
      * Map the given results to instances of the given model.
      *
+     * @param  \Laravel\Scout\Builder  $builder
      * @param  mixed  $results
      * @param  \Illuminate\Database\Eloquent\Model  $model
      * @return \Illuminate\Database\Eloquent\Collection
      */
-    public function map($results, $model)
+    public function map(Builder $builder, $results, $model)
     {
         return Collection::make();
     }
