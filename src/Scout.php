1129a0b8755bc19bec0442cdb5188e99367a4da4
Matan Yadaev <matan.yed@gmail.com>
2021-06-01 17:29:28 +0300

    add phpDoc

--- src/Scout.php
+++ src/Scout.php
@@ -7,7 +7,17 @@ use Laravel\Scout\Jobs\RemoveFromSearch;
 
 class Scout
 {
+    /**
+     * The make searchable job name.
+     *
+     * @var string
+     */
     public static $makeSearchableJob = MakeSearchable::class;
 
+    /**
+     * The remove from search job name.
+     *
+     * @var string
+     */
     public static $removeFromSearchJob = RemoveFromSearch::class;
 }
