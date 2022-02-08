2943b19930fbab402b9b60e58890430a3bad1db9
Taylor Otwell <taylor@laravel.com>
2022-02-08 09:51:46 -0600

    wip

--- config/scout.php
+++ config/scout.php
@@ -11,7 +11,7 @@ return [
     | using Laravel Scout. This connection is used when syncing all models
     | to the search service. You should adjust this based on your needs.
     |
-    | Supported: "algolia", "meilisearch", "collection", "null"
+    | Supported: "algolia", "meilisearch", "database", "collection", "null"
     |
     */
 
