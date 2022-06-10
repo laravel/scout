16f47d7d8d15d89ecf5a2699601d15c62f0c2545
Marcin Derlukiewicz <m.derlukiewicz@expondo.pl>
2022-06-10 13:03:30 +0200

    fix ci

--- tests/Unit/CollectionEngineTest.php
+++ tests/Unit/CollectionEngineTest.php
@@ -4,12 +4,8 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\LazyCollection;
 use Laravel\Scout\Builder;
-use Laravel\Scout\Engines\AlgoliaEngine;
 use Laravel\Scout\Engines\CollectionEngine;
-use Laravel\Scout\Tests\Fixtures\EmptySearchableModel;
-use Laravel\Scout\Tests\Fixtures\SearchableModel;
 use Laravel\Scout\Tests\Fixtures\SearchableModelWithCustomKeyForCollectionEngineTest;
-use Laravel\Scout\Tests\Fixtures\SoftDeletedEmptySearchableModel;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
@@ -32,7 +28,7 @@ class CollectionEngineTest extends TestCase
         $engine = new CollectionEngine();
         $model = new SearchableModelWithCustomKeyForCollectionEngineTest([
             'id' => 1,
-            'other_id' => 1234
+            'other_id' => 1234,
         ]);
         $modelMock = m::mock(SearchableModelWithCustomKeyForCollectionEngineTest::class);
         $modelMock->shouldReceive('getScoutModelsByIds')->withArgs([$builder, [1234]])
@@ -42,7 +38,7 @@ class CollectionEngineTest extends TestCase
         $modelMock->shouldReceive('getScoutKeyName')->andReturn('other_id');
 
         $results = $engine->map($builder, [
-            'results' => [$model]
+            'results' => [$model],
         ], $modelMock);
 
         $this->assertCount(1, $results);
@@ -54,7 +50,7 @@ class CollectionEngineTest extends TestCase
         $engine = new CollectionEngine();
         $model = new SearchableModelWithCustomKeyForCollectionEngineTest([
             'id' => 1,
-            'other_id' => 1234
+            'other_id' => 1234,
         ]);
         $builder = m::mock(Builder::class);
         $builder->shouldReceive('cursor')->andReturn(LazyCollection::make([$model]));
@@ -64,7 +60,7 @@ class CollectionEngineTest extends TestCase
         $modelMock->shouldReceive('getScoutKeyName')->andReturn('other_id');
 
         $results = $engine->lazyMap($builder, [
-            'results' => [$model]
+            'results' => [$model],
         ], $modelMock);
 
         $this->assertCount(1, $results);
