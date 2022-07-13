86fa31b4fa718f29f02fae7fe7c72e2a61212d2c
Franky So <frankyso.mail@gmail.com>
2022-07-13 17:14:17 +0700

    Fixed meilisarch breaking change

--- src/Engines/MeiliSearchEngine.php
+++ src/Engines/MeiliSearchEngine.php
@@ -296,7 +296,7 @@ class MeiliSearchEngine extends Engine
      */
     public function getTotalCount($results)
     {
-        return $results['nbHits'];
+        return $results['estimatedTotalHits'];
     }
 
     /**
