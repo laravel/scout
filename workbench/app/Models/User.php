942d235db051fd9cd523dc7db9a154ad27f0c1e0
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-12 17:00:35 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- workbench/app/Models/User.php
+++ workbench/app/Models/User.php
@@ -41,6 +41,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        // 'password' => 'hashed',
     ];
 }
