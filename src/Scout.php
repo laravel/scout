c976992a5202465743d47880ebde9dcac198dff5
Taylor Otwell <taylor@laravel.com>
2025-10-09 16:02:11 -0500

    add engine helper

--- src/Scout.php
+++ src/Scout.php
@@ -2,6 +2,8 @@
 
 namespace Laravel\Scout;
 
+use Laravel\Scout\EngineManager;
+use Laravel\Scout\Engines\Engine;
 use Laravel\Scout\Jobs\MakeSearchable;
 use Laravel\Scout\Jobs\RemoveFromSearch;
 
@@ -27,6 +29,14 @@ class Scout
      * @var string
      */
     public static $removeFromSearchJob = RemoveFromSearch::class;
+
+    /**
+     * Get a Scout engine instance.
+     */
+    public static function engine(string $engine): Engine
+    {
+        return app(EngineManager::class)->engine($engine);
+    }
 
     /**
      * Specify the job class that should make models searchable.
