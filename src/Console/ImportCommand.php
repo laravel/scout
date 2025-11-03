f314e7e45ff3a02fe4c598c04284f0e15cf59eff
Taylor Otwell <taylor@laravel.com>
2025-11-03 15:51:58 -0600

    formatting

--- src/Console/ImportCommand.php
+++ src/Console/ImportCommand.php
@@ -41,7 +41,7 @@ class ImportCommand extends Command
         $class = $this->argument('model');
 
         if (! class_exists($class) && ! class_exists($class = app()->getNamespace()."Models\\{$class}")) {
-            throw new ScoutException("Error: Model [{$class}] not found.");
+            throw new ScoutException("Model [{$class}] not found.");
         }
 
         $model = new $class;
