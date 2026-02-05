f58664ea267d7add72adf9fd6b14691dafc19f59
Jamie York <ziadoz@gmail.com>
2026-02-05 21:10:16 +0000

    wip

--- src/Traits/ConfiguresJob.php
+++ src/Traits/ConfiguresJob.php
@@ -5,6 +5,20 @@ namespace Laravel\Scout\Traits;
 trait ConfiguresJob
 {
     /**
+     * The number of times the job may be attempted.
+     *
+     * @var int|null
+     */
+    public $tries;
+
+    /**
+     * The number of seconds to wait before retrying the job.
+     *
+     * @var int|array<int>|null
+     */
+    public $backoff;
+
+    /**
      * Configure the job from Scout's configuration.
      *
      * @return void
