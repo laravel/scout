50e8e65a2606d0e4da733dd10fea1cdde4406ffd
Mior Muhammad Zaki <crynobone@gmail.com>
2026-02-06 09:54:41 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Traits/ConfiguresJobOptions.php
+++ src/Traits/ConfiguresJobOptions.php
@@ -12,17 +12,20 @@ trait ConfiguresJobOptions
     protected function configureJob(): void
     {
         if (! isset($this->tries) && ! is_null($tries = config('scout.jobs.tries'))) {
+            /** @phpstan-ignore property.notFound */
             $this->tries = $tries;
         }
 
         if (! isset($this->backoff) &&
             ! method_exists($this, 'backoff') &&
             ! is_null($backoff = config('scout.jobs.backoff'))) {
+            /** @phpstan-ignore property.notFound */
             $this->backoff = $backoff;
         }
 
         if (! isset($this->maxExceptions) &&
             ! is_null($maxExceptions = config('scout.jobs.max_exceptions'))) {
+            /** @phpstan-ignore property.notFound */
             $this->maxExceptions = $maxExceptions;
         }
     }
