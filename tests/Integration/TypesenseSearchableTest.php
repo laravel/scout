2b28fcf15c3743d68eef2981e902505ceaf78deb
Günther Debrauwer <gunther.debrauwer@whoownsthezebra.be>
2026-02-25 15:32:38 +0100

    small change

--- tests/Integration/TypesenseSearchableTest.php
+++ tests/Integration/TypesenseSearchableTest.php
@@ -42,7 +42,7 @@ class TypesenseSearchableTest extends TestCase
                     ],
                     [
                         'name' => 'age',
-                        'type' => 'auto',
+                        'type' => 'int64',
                     ],
                 ],
             ],
