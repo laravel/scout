169602c5b59bf4b7bebdb3f6f2539262a6f0293b
Mior Muhammad Zaki <crynobone@gmail.com>
2026-02-25 21:16:14 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- workbench/database/factories/UserFactory.php
+++ workbench/database/factories/UserFactory.php
@@ -36,6 +36,7 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'age' => fake()->numberBetween(18, 90),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
