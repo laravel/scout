ac93020ec38bd343fa844276f9efe365d1c1fc31
Taylor Otwell <taylor@laravel.com>
2026-07-21 09:35:31 -0500

    Update Scout.php

--- src/Scout.php
+++ src/Scout.php
@@ -13,7 +13,7 @@ class Scout
      *
      * @var string
      */
-    const VERSION = '10.27.0';
+    const VERSION = '10.28.0';
 
     /**
      * The job class that should make models searchable.
